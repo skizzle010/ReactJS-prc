247969e72864e70aba571d8f257b5f643261f0fc
skizzle010 <raiutkarsh3219@gmail.com>
2023-08-15 22:34:02 +0530

    feat:profile card

--- 04-profile-card/src/index.js
+++ 04-profile-card/src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Image />
+      <div className="data">
+        <h1>Hecker catto</h1>
+        <p>
+          Just a catto chilling in da house. Want to eat food and sleep all day
+          :3 I have only one job to do meow meow all day.
+        </p>
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+const Image = () => {
+  return <img className="avatar" src="/catto.png" alt="cat" />;
+};
+
+const Skill = (props) => {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+
+      <backgroudn></backgroudn>
+    </div>
+  );
+};
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="hecking" color="blue" emoji="💀" />
+      <Skill skill="cuteness" color="red" emoji="😏" />
+      <Skill skill="big boy" color="yellow" emoji="😈" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
