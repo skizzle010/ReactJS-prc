55768933da3ab0cda5f646e44cea4ee140e3af5d
skizzle010 <raiutkarsh3219@gmail.com>
2023-08-16 04:30:51 +0530

    fix: footer

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -71,70 +71,47 @@ const Footer = () => {
   const closeHour = 22;
   const isOpen = hour >= openHour && hour < closeHour;
   console.log(isOpen);
+  const pizzas = pizzaData;
   //   if (hour >= openHour && hour < closeHour) alert("We're currently open");
   //   else alert("Sorry We are closed!!");
-  if (isOpen) {
-    return (
-      <footer className="footer">
-        {new Date().toLocaleTimeString()}. We are currently Open!!
-      </footer>
-    );
-  } else {
-    return (
-      <footer>{new Date().toLocaleTimeString()}. Sorry We are closed!!</footer>
-    );
-  }
+
+  return (
+    <footer className="footer">
+      {pizzas.length > 0 ? (
+        <div>
+          {isOpen ? (
+            <div className="order">
+              <p>
+                We're currently open until {closeHour}:00. Come visit us or
+                order online.
+              </p>
+              <button className="btn">Order Now!</button>
+            </div>
+          ) : (
+            <p>Sorry we are closed!!</p>
+          )}
+        </div>
+      ) : null}
+    </footer>
+  );
 };
 
 const Menu = () => {
+  // const pizzas=[]
+  const pizzas = pizzaData;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <div className="pizza">
-        <Pizza
-          name="Pizza Spinaci"
-          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-          photoName="/pizzas/spinaci.jpg"
-          price={10}
-          soldOut={false}
-        />
-
-        <Pizza
-          name="Pizza Funghi"
-          ingredients="Tomato, mozarella, mushrooms, and onion"
-          price={12}
-          photoName="pizzas/funghi.jpg"
-          soldOut={false}
-        />
-        <Pizza
-          name="Pizza Margherita"
-          ingredients="Tomato and mozarella"
-          photoName="pizzas/margherita.jpg"
-          price={10}
-          soldOut={false}
-        />
-        <Pizza
-          name="Focaccia"
-          ingredients="Bread with italian olive oil and rosemary"
-          photoName="pizzas/focaccia.jpg"
-          price={6}
-          soldOut={false}
-        />
-        <Pizza
-          name="Pizza Salamino"
-          ingredients="Tomato, mozarella, and pepperoni"
-          photoName="pizzas/salamino.jpg"
-          price={15}
-          soldOut={true}
-        />
-        <Pizza
-          name="Pizza Prosciutto"
-          ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-          photoName="pizzas/prosciutto.jpg"
-          price={18}
-          soldOut={false}
-        />
-      </div>
+      {pizzas.length > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza {...pizza} />
+          ))}
+        </ul>
+      ) : (
+        <p>Sorry, we're currently sold out</p>
+      )}
     </main>
   );
 };
@@ -143,11 +120,11 @@ const Pizza = (props) => {
   return (
     <div className="pizza">
       <img src={props.photoName} alt={props.name} />
-      <div>
+      <li>
         <h3>{props.name}</h3>
         <p>{props.ingredients}</p>
         <span>{props.price}$</span>
-      </div>
+      </li>
     </div>
   );
 };
