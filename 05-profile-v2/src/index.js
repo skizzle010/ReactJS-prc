6603db204fb3778da47dbf1a84163cb5ef141f70
skizzle010 <raiutkarsh3219@gmail.com>
2023-08-17 04:06:05 +0530

    feat:profilev2 setup

---
+++ 05-profile-v2/src/index.js
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const App = () => {
+  return <p>Hello</p>;
+};
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
