5938b9073ff5eb5237ca9584b1234a845293df66
skizzle010 <raiutkarsh3219@gmail.com>
2023-08-20 02:59:15 +0530

    feat: count-step/date-calc

--- 07-date-calculator/src/App.js
+++ 07-date-calculator/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <>
@@ -8,20 +10,50 @@ function App() {
 
 const Step = () => {
   const [step, setStep] = useState(1);
+  const [count, setCount] = useState(1);
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  function handleStepInc() {
+    setStep((s) => s + 1);
+  }
+  function handleStepDec() {
+    setStep((s) => s - 1);
+  }
+
+  function handleCountInc() {
+    setCount((s) => s + step);
+  }
+  function handleCountDec() {
+    setCount((s) => s - step);
+  }
+
   return (
     <>
       <div className="component center-row">
-        <button className="button">-</button>
-        <h1>Step: 1</h1>
-        <button className="button">+</button>
+        <button className="button" onClick={handleStepDec}>
+          -
+        </button>
+        <h1>Step: {step}</h1>
+        <button className="button" onClick={handleStepInc}>
+          +
+        </button>
       </div>
       <div className="component center-row">
-        <button className="button">-</button>
-        <h1>Count: 1</h1>
-        <button className="button">+</button>
+        <button className="button" onClick={handleCountDec}>
+          -
+        </button>
+        <h1>Count: {count}</h1>
+        <button className="button" onClick={handleCountInc}>
+          +
+        </button>
       </div>
-      <div>
-        <p>Today is Mon Jun 21 2027 </p>
+      <div className="container">
+        <p className="date-text">
+          <span></span>
+          <span>{date.toDateString()}</span>
+        </p>
       </div>
     </>
   );
