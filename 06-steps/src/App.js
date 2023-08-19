4b5778afd860b80ac85481a9f9f9cf2d8013a843
skizzle010 <raiutkarsh3219@gmail.com>
2023-08-19 22:41:55 +0530

    fix: useState

--- 06-steps/src/App.js
+++ 06-steps/src/App.js
@@ -24,9 +24,9 @@ const App = () => {
     (
       <div className="steps">
         <div className="numbers">
-          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
         </div>
         <p className="message">
           Step {step} : {messages[step - 1]}
